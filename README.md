4e2c5f02ffbaaf800cb15fec957fe25269186fd8
Pank0 <panici.k@gmail.com>
2020-03-22 12:24:02 +0100

    Trying to create the web app

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 Refs @ http://ichatz.me/Site/InternetOfThings2020-Assignment1
 
 ## Requirements:
+### /virtual-sensors
 - pahp-mqtt @ https://pypi.org/project/paho-mqtt/
+
+### /Web_View
+- django @ https://www.djangoproject.com/
--- virtual-sensors/Simulation.py
+++ virtual-sensors/Simulation.py
@@ -17,21 +17,25 @@ env_station_A = EnvironmentalStation(temperature_sensor_A, humidity_sensor_A,
                                      wind_dir_sensor_A, wind_int_sensor_A,
                                      rain_height_sensor_A)
 
-ACCESS_TOKEN='vgFztmvT6bps7JCeOEZq'                 # Token of your device
+# Communication
+ACCESS_TOKEN_A='vgFztmvT6bps7JCeOEZq'               # Token of your device A
 broker="demo.thingsboard.io"                        # host name
 port=1883                                           # data listening port
 
 def on_publish(client,userdata,result):             # create function for callback
     print("data published to thingsboard \n")
     pass
-client1= paho.Client("control1")                    # create client object
-client1.on_publish = on_publish                     # assign function to callback
-client1.username_pw_set(ACCESS_TOKEN)               # access token from thingsboard device
-client1.connect(broker,port,keepalive=60)           # establish connection
 
-while True:  
+# Setting Up Client A
+client_A= paho.Client("control1")                    # create client object
+client_A.on_publish = on_publish                     # assign function to callback
+client_A.username_pw_set(ACCESS_TOKEN_A)               # access token from thingsboard device
+client_A.connect(broker,port,keepalive=60)           # establish connection
+
+while True:
+    
    payload = env_station_A.getPayload()
-   ret= client1.publish("v1/devices/me/telemetry",payload) # topic-v1/devices/me/telemetry
+   ret= client_A.publish("v1/devices/me/telemetry",payload) # topic-v1/devices/me/telemetry
    print("Please check LATEST TELEMETRY field of your device")
    print(payload);
-   time.sleep(5)
+   time.sleep(2)
