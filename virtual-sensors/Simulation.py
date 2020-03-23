4d81cb91c00a40c3334bf0d519d37d4fa5288537
Pank0 <panici.k@gmail.com>
2020-03-23 16:13:05 +0100

    a genius

--- virtual-sensors/Simulation.py
+++ virtual-sensors/Simulation.py
@@ -29,10 +29,11 @@ env_station_B = EnvironmentalStation(temperature_sensor_B, humidity_sensor_B,
                                      rain_height_sensor_B)
 
 # Communication
-ACCESS_TOKEN_A = 'vgFztmvT6bps7JCeOEZq'               # Token of device A
-ACCESS_TOKEN_B = 'BKJK0j5zVyh3hIkCBWDI'               # Token of device B
-broker = "demo.thingsboard.io"                        # host name
-port = 1883                                           # data listening port
+ACCESS_TOKEN_A = 'vgFztmvT6bps7JCeOEZq'                # Token of device A
+ACCESS_TOKEN_B = 'BKJK0j5zVyh3hIkCBWDI'                # Token of device B
+broker = "demo.thingsboard.io"                         # host name
+topic = "v1/devices/me/telemetry"
+port = 1883                                       # data listening port
 
 def on_publish(client,userdata,result):             # create function for callback
     print("data published to thingsboard \n")
@@ -50,20 +51,19 @@ client_B.on_publish = on_publish                     # assign function to callba
 client_B.username_pw_set(ACCESS_TOKEN_B)               # access token from thingsboard device
 client_B.connect(broker,port,keepalive=60)           # establish connection
 
+
 while True:
     
-   payload_A = env_station_A.getPayload()
-   ret = client_A.publish("v1/devices/me/telemetry",payload_A) # topic-v1/devices/me/telemetry
-   print("AAA ENVIRONMENTAL STATION A ")
-   print(payload_A);
-   print ("\n")
-   
-   time.sleep(2)
-   
-   payload_B = env_station_B.getPayload()
-   ret = client_B.publish("v1/devices/me/telemetry",payload_B)
-   print("BBB ENVIRONMENTAL STATION B ")
-   print(payload_B)
-   print ("\n")
-   
-   time.sleep(2)
+    payload_A = env_station_A.getPayload()
+    ret = client_A.publish(topic, payload_A) # topic-v1/devices/me/telemetry
+    print("AAA ENVIRONMENTAL STATION A ")
+    print(payload_A)
+    print ("\n")
+
+    payload_B = env_station_B.getPayload()
+    ret = client_B.publish(topic, payload_B)
+    print("BBB ENVIRONMENTAL STATION B ")
+    print(payload_B)
+    print ("\n")
+
+    time.sleep(2)
