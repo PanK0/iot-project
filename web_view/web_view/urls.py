033206d4371c7297519d125c6a09f45e0bec8d33
Pank0 <panici.k@gmail.com>
2020-03-23 18:16:29 +0100

    Web app adjustments

--- web_view/web_view/urls.py
+++ web_view/web_view/urls.py
@@ -7,5 +7,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),                    # admin panel route
     path('', views.home, name='web_view_home'),         # general home page
-    path('blog/', include('blog.urls')),                # blog home page
+    path('blog/', include('blog.urls'), name='blog-home'),                # blog home page
 ]
