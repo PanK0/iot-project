7a8ba1b5655fec8e58e3f6c7cd22db179b0fd641
Pank0 <panici.k@gmail.com>
2020-03-24 10:46:10 +0100

    stuffs

--- web_view/web_view/settings.py
+++ web_view/web_view/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iw&fkx+ae(ct)-!b5h-za*3h!3slu!i)0w9bvb6))zzw)*z+xo'
+# SECRET_KEY = 'iw&fkx+ae(ct)-!b5h-za*3h!3slu!i)0w9bvb6))zzw)*z+xo'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iotwebapp.herokuapp.com']
 
 
 # Application definition
