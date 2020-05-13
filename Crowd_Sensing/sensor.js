758bdfd9c976efebeffd303b9a1baf187640e363
Pank0 <panici.k@gmail.com>
2020-05-13 12:47:56 +0200

    test

--- Crowd_Sensing/sensor.js
+++ Crowd_Sensing/sensor.js
@@ -1,1 +1,18 @@
- 
+let xval = document.getElementById('x_value');
+let yval = document.getElementById('y_value');
+let zval = document.getElementById('z_value');
+
+$(document).ready(() => {
+  try {
+    if (window.Accelerometer) {
+      document.getElementById("div-error").innerHTML = 'This stuff seems to work';
+    } else {
+      console.log({ error: "Generic API Accelerometer not available" });
+      document.getElementById("div-error").innerHTML = 'NOT WORKING';
+    }
+  } catch (error) {
+    console.log({ error: error });
+    document.getElementById("div-error").innerHTML = 'Errrrrrr';
+
+  }
+});
